1b005ff3fefec40174316ff75b909d267e52bd6b
ggmlduswjs <sjrnf5749@gmail.com>
2026-01-02 17:46:00 +0900

    remove: 寃곗젣 ?뚰듃 愿???뚯씪 ?꾩껜 ??젣
    - payment ?⑦궎吏 ?꾩껜 ??젣 (card, kakao, common)
    - cart ?⑦궎吏 ?꾩껜 ??젣
    - 寃곗젣 愿???쒗뵆由??뚯씪 ??젣
    - 寃곗젣 愿???뺤쟻 ?뚯씪 ??젣 (CSS, JS)
    - 寃곗젣 愿??留ㅽ띁 XML ?뚯씪 ??젣
    - 留덉씠?섏씠吏 寃곗젣 愿???쒕퉬??諛?DTO ??젣
    - 寃곗젣 ?뚰듃??蹂꾨룄濡?癒몄? ?덉젙

--- src/main/java/com/learnit/learnit/payment/common/LoginRequiredException.java
+++
@@ -1,8 +0,0 @@
-package com.learnit.learnit.payment.common;
-
-public class LoginRequiredException extends PaymentException {
-
-    public LoginRequiredException(String message) {
-        super(message);
-    }
-}
